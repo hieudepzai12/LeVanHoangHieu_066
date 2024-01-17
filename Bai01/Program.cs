80bf65abc297e36f7a97e66d65a160f83ecac381
ADMIN <ADMIN@PC348>
2024-01-17 10:27:37 +0700

    Add project files.

---
+++ Bai01/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai01
+{
+    class SinhVien
+    {
+        private string Hoten;
+        private double DiemToan;
+        private double DiemVan;
+        private double DiemNgoaiNgu;
+        private string KhoiHoc;
+        
+        public SinhVien()
+        {
+
+        }
+        public SinhVien(string Hoten, double DiemToan, double DiemVan, double DiemNgoaiNgu, string KhoiHoc)
+        {
+            this.Hoten = Hoten;
+            this.DiemToan = DiemToan;
+            this.DiemVan = DiemVan;
+            this.DiemNgoaiNgu = DiemNgoaiNgu;
+            this.KhoiHoc = KhoiHoc;
+        }
+        public string HoTen
+        {
+            set { this.Hoten = value; }
+            get { return Hoten; }
+        }
+        public double diemToan
+        {
+            set { this.DiemToan = value; }
+            get { return DiemToan; }
+        }
+        public double diemVan
+        {
+            set { this.DiemVan = value; }
+            get { return DiemVan; }
+        }
+        public double diemNgoaiNgu
+        {
+            set { this.DiemNgoaiNgu= value; }
+            get { return DiemNgoaiNgu; }
+        }
+        public string khoiHoc
+        {
+            set { this.KhoiHoc = value; }
+            get { return KhoiHoc; }
+        }
+        public double TinhTrungBinh()
+        {
+            double heso = 1;
+            switch (KhoiHoc)
+            {
+                case "A":
+                    heso = 2;
+                    break;
+                case "D":
+                    heso = 2;
+                    break;
+                case "C":
+                    heso = 2;
+                    break;
+            }
+            return (DiemToan * heso + DiemVan * heso + heso * DiemNgoaiNgu) / (3 * heso);
+        }
+        public string TinhXepLoai()
+        {
+            if (TinhTrungBinh() >= 5)
+            {
+                return "Duoc len lop";
+            }
+            else
+            {
+                return "Luu ban";
+            }
+        }
+        public void InThongTin()
+        {
+            Console.WriteLine("Ho ten sinh vien: {0}", Hoten);
+            Console.WriteLine("Diem trung binh: {0}", TinhTrungBinh());
+            Console.WriteLine(" Xep loai: {0}", TinhXepLoai());
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SinhVien sv1 = new SinhVien();
+
+            Console.WriteLine("Nhap ho ten sinh vien: ");
+            sv1.HoTen = Console.ReadLine();
+            Console.WriteLine(" Nhap diem toan: ");
+            sv1.diemToan = double.Parse(Console.ReadLine());
+            Console.WriteLine(" Nhap diem van: ");
+            sv1.diemVan = double.Parse(Console.ReadLine());
+            Console.WriteLine(" Nhap diem ngoai ngu: ");
+            sv1.diemNgoaiNgu = double.Parse(Console.ReadLine());
+            Console.WriteLine(" Nhap khoi Hoc: ");
+            //sv1.khoiHoc = Console.ReadLine();
+            //Console.WriteLine("Thong tin sinh vien 1: ");
+            sv1.InThongTin();
+            SinhVien sv2 = new SinhVien("Nguyen Van A", 7, 5, 4, "A");
+            Console.WriteLine("Thong tin sinh vien 2:");
+            sv2.InThongTin();
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Bai02/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai02
+{
+    class ChuNhat
+    {
+        private double _length;
+        private double _width;
+        public ChuNhat()
+        {
+
+        }
+        public ChuNhat(double length, double width)
+        {
+            this._length = length;
+            this._width = width;
+        }
+        public double Length
+        {
+            set { this._length = value; }
+            get { return _length; }
+        }
+        public double Width
+        {
+            set { this._width = value; }
+            get { return _width; }
+        }
+        public double TinhChuVi()
+        {
+            return (_length + _width) * 2;
+        }
+        public double TinhDienTich()
+        {
+            return _length * _width;
+        }
+        public void Xuat()
+        {
+            Console.WriteLine("Thong tin hinh chu nhat:");
+            Console.WriteLine("Chu vi hinh chu nhat: {0}", TinhChuVi());
+            Console.WriteLine("Dien tich hinh chu nhat: {0}", TinhDienTich());
+        }
+    }
+        class Vuong: ChuNhat
+        {
+            private double canh;
+            public Vuong()
+            {
+
+            }
+            public Vuong(double canh)
+            {
+                this.canh = canh;
+            }
+            public double Canh
+            {
+                set { this.canh = value; }
+                get { return canh; }
+            }
+            public double TinhChuVi()
+            {
+                return canh * 4;
+            }
+            public double TinhDienTich()
+            {
+                return canh * canh;
+            }
+            public void Xuat()
+            {
+            Console.WriteLine("Thong tin hinh vuong:");
+            Console.WriteLine("Chu vi hinh vuong: {0}", TinhChuVi());
+            Console.WriteLine("Dien tich hinh vuong: {0}", TinhDienTich());
+            }
+        }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChuNhat chunhat = new ChuNhat();
+            Console.WriteLine("Nhap chieu dai: ");
+            chunhat.Length = double.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap chieu rong: ");
+            chunhat.Width = double.Parse(Console.ReadLine());
+            chunhat.Xuat();
+            //Console.WriteLine("Thong tin hinh chu nhat:");
+            //Console.WriteLine("Chu vi hinh chu nhat: {0}", chunhat.TinhChuVi());
+            //Console.WriteLine("Dien tich hinh chu nhat: {0}", chunhat.TinhDienTich());
+
+            Vuong vuong = new Vuong();
+            Console.WriteLine("Nhap canh: ");
+            vuong.Canh = double.Parse(Console.ReadLine());
+            vuong.Xuat();
+            //Console.WriteLine("Thong tin hinh vuong:");
+            //Console.WriteLine("Chu vi hinh vuong: {0}", vuong.TinhChuVi());
+            //Console.WriteLine("Dien tich hinh vuong: {0}", vuong.TinhDienTich());
+
+            Console.ReadKey();
+        }
+    }
+}
